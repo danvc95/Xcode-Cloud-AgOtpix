542c1ae8dfd5b9c606003b20fbf190022ef87361
Daniel Van Cleave <dinovc@icloud.com>
2023-02-24 15:30:14 -0800

    updated the code

---
+++ Ag  Optix/Ag  Optix/Ag__OptixApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ag__OptixApp.swift
+//  Ag  Optix
+//
+//  Created by Daniel Van Cleave on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Ag__OptixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ag  Optix/Ag  Optix/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Ag  Optix
+//
+//  Created by Daniel Van Cleave on 2/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, xcode cloud!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
